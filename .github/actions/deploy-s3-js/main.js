a7b60f10e7b1103c3509289c92845d1a4d496d27
Vansh Goel <vansh.eigenstudio@gmail.com>
2026-01-13 19:52:03 +0530

    added website url

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -10,7 +10,7 @@ async function run() {
     const s3uri= `s3://${bucket}/`
     exec.exec(`aws s3 sync ${distFolder} ${s3uri} --region ${bucketRegion}`)
 
-
-    core.notice('Hello from my custom js action');
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('deploy-url', websiteUrl);
 }
 run();
