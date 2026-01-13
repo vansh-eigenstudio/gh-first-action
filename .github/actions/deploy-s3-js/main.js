ccd53251b30db017e99c10d3dbafb44bb4faa8f5
Vansh Goel <vansh.eigenstudio@gmail.com>
2026-01-13 19:42:58 +0530

    added custom deploy to s3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,8 +1,16 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 async function run() {
+    // Get inputs defined in action.yml file
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+    //Upload the files to s3 bucket
+    const s3uri= `s3://${bucket}/`
+    exec.exec(`aws s3 sync ${distFolder} ${s3uri} --region ${bucketRegion}`)
+
+
     core.notice('Hello from my custom js action');
 }
 run();
